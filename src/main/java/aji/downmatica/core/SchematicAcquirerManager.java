4f60591da6ef5ed13a6fae0d321b3af6f7a0f45d
aji <2442733838@qq.com>
2026-06-16 00:35:59 +0800

    更新java文档

--- src/main/java/aji/downmatica/core/SchematicAcquirerManager.java
+++ src/main/java/aji/downmatica/core/SchematicAcquirerManager.java
@@ -16,12 +16,12 @@ public class SchematicAcquirerManager {
     }
 
     /**
-     * 此方法功内部调用，不推荐直接使用。
+     * 此方法供内部调用，不推荐直接使用。
      * <p>
      * 未来版本中，该方法的签名或行为可能会在不预先通知的情况下发生变化。
      * <p>
      * 建议使用{@link SchematicAcquirer#register(SchematicAcquirer)}作为替代，
-     * 该 API 会保持向下兼容性，而此方法不会。
+     * 该方法会保持向下兼容性，而此方法不会。
      */
     public void register(SchematicAcquirer acquirer) {
         acquirers.add(acquirer);
