2fbcc838a79c37184f31bc9fce0b7e4f52817c07
aji <2442733838@qq.com>
2026-07-16 16:50:58 +0800

    修复当开启downloadToSchematicBaseDirectory并且没有安装Litematica模组时点击下载按钮时会崩溃的问题

--- src/main/java/top/ajitech/downmatica/gui/DownloadGui.java
+++ src/main/java/top/ajitech/downmatica/gui/DownloadGui.java
@@ -5,6 +5,7 @@ import fi.dy.masa.malilib.gui.*;
 import top.ajitech.downmatica.Downmatica;
 import top.ajitech.downmatica.api.Schematic;
 import top.ajitech.downmatica.api.SchematicDownloadInfo;
+import top.ajitech.downmatica.compatible.LitematicaCompatible;
 import top.ajitech.downmatica.core.ConfigHandler;
 import top.ajitech.downmatica.core.HttpClientContainer;
 import top.ajitech.downmatica.core.SchematicAcquirerManager;
@@ -325,7 +326,7 @@ public class DownloadGui extends GuiListBase<Schematic, DownloadGui.WidgetList.E
                         new ButtonGeneric(x, y, downloadButtonWidth, buttonHeight, downloadButtonDisplay),
                         (button, mouseButton) -> Thread.ofVirtual().name("SchematicDownloader").start(() -> {
                             Path path;
-                            if (ConfigHandler.INSTANCE.downloadToSchematicBaseDirectory.getBooleanValue()) {
+                            if (ConfigHandler.INSTANCE.downloadToSchematicBaseDirectory.getBooleanValue() && LitematicaCompatible.isLitematicaInstalled()) {
                                 //#if MC < 1216
                                 path = DataManager.getSchematicsBaseDirectory().toPath();
                                 //#else
